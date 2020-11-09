75ba39e0120b414f6b5e88ef35961c18b058bf26
sanket <sanketgargate06@gmail.com>
2020-11-09 22:30:02 +0530

    R-2.9

---
+++ Data_Structures_and_Algorithms_in_Python_GoodRich/Chapter_2_Object-Oriented_Programming/Reinforcement/2.9.py
@@ -0,0 +1,83 @@
+"""
+2.9 Implement the sub method for the Vector class of Section 2.3.3, so
+that the expression u−v returns a new vector instance representing the
+difference between two vectors.
+
+2.10 Implement the neg method for the Vector class of Section 2.3.3, so
+that the expression −v returns a new vector instance whose coordinates
+are all the negated values of the respective coordinates of v.
+
+2.11 In Section 2.3.3, we note that our Vector class supports a syntax such as
+v = u + [5, 3, 10, −2, 1], in which the sum of a vector and list returns
+a new vector. However, the syntax v = [5, 3, 10, −2, 1] + u is illegal.
+Explain how the Vector class definition can be revised so that this syntax
+generates a new vector.
+"""
+class Vector:
+    """ Represent a vector in a multidimensional space."""
+
+    def __init__(self, d):
+        """Create d-dimensional vector of zeros."""
+        self._coords = [0] * d
+
+    def __len__(self):
+        """ Return the dimension of the vector"""
+        return len(self._coords)
+
+    def __getitem__(self, j):
+        """Return jth coordinate of vector."""
+        return self._coords[j]
+
+    def __setitem__(self, j, val):
+        """ Set jth coordinate of vector to given value"""
+        self._coords[j] = val
+
+    def __add__(self, other):
+        """Return sum of two vectors."""
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    def __eq__(self, other):
+        """Return True if vector has same coordinates as other."""
+        return self._coords == other._coords
+
+    def __ne__(self, other):
+        """Return True if vector differs from other."""
+        return not self == other
+
+    def __str__(self):
+        """Produce string representation of vector."""
+        return '<' + str(self._coords)[1:-1] + '>'
+
+    def __sub__(self, other):
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] - other[j]
+        return result
+
+    def __neg__(self):
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = -self[j]
+        return result
+
+if __name__ == '__main__':
+    v1 = Vector(3)
+    v1Values = [10,20,30]
+    for j in range(len(v1)):
+        v1[j] = v1Values[j]
+    u1 = Vector(3)
+    u1Values = [5,15,25]
+    for j in range(len(u1)):
+        u1[j] = u1Values[j]
+    lenU1 = len(u1Values)
+    print(v1)
+    print(u1)
+    print(v1.__add__(u1))
+    print(u1.__sub__(v1))
