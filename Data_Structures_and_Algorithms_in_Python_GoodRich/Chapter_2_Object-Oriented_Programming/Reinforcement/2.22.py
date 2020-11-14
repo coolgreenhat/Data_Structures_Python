db56e9d830645584b05313d498b0f98c4cadba58
sanket <sanketgargate06@gmail.com>
2020-11-14 21:29:40 +0530

    R-2.23

--- Data_Structures_and_Algorithms_in_Python_GoodRich/Chapter_2_Object-Oriented_Programming/Reinforcement/2.22.py
+++ Data_Structures_and_Algorithms_in_Python_GoodRich/Chapter_2_Object-Oriented_Programming/Reinforcement/2.22.py
@@ -1,3 +1,13 @@
+"""
+2.22 The collections.Sequence abstract base class does not provide support for
+comparing two sequences to each other. Modify our Sequence class from
+Code Fragment 2.14 to include a definition for the eq method, so
+that expression seq1 == seq2 will return True precisely when the two
+sequences are element by element equivalent.
+
+2.23 In similar spirit to the previous problem, augment the Sequence class with
+method lt , to support lexicographic comparison seq1 < seq2.
+"""
 from abc import ABCMeta, abstractmethod
 
 
@@ -38,5 +48,13 @@ class Sequence(metaclass=ABCMeta):
             raise ValueError('dimensions must agree')
         for j in range(len(self)):
             if self[j] == other[j] :
+                return False
+        return True
+
+    def __lt__(self, other):
+        if len(self) != len(other):
+            raise ValueError('dimensions must agree')
+        for j in range(len(self)):
+            if self[j]  > other[j] :
                 return False
         return True
